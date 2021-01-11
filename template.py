ac1ffff6289262c2b51948e5a608e443df170067
Noritaka IZUMI <noritaka.izumi@gmail.com>
2021-01-11 18:47:54 +0900

    Initial commit.

---
+++ template.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# 問題番号 - 問題のタイトル
+
+"""
+問題文の概要。
+"""
+
+
+def main():
+    pass
+
+
+def test():
+    assert 1 == 1
+
+
+if __name__ == '__main__':
+    main()
+    # test()
